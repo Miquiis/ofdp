95ff8a290ae5eb1a6ca2a25c2663c199713202ff
Miquiis <miquiisoficial@gmail.com>
2021-10-07 19:25:24 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('build'));
+    app.get('*', function(req, res) {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
+
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('Server running on port ' + port);
+});
